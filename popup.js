1f389a561759a647ee8330f527b434ec274f4637
varun77x <varun77x@gmail.com>
2025-01-29 03:49:35 +0530

    initial commit

---
+++ popup.js
@@ -0,0 +1,23 @@
+document.getElementById('openQuestion').addEventListener('click', () => {
+    const questionNumber = document.getElementById('questionNumber').value;
+    if (questionNumber) {
+      // Load the questions.json file
+      fetch('questions.json')
+        .then(response => response.json())
+        .then(data => {
+          const slug = data[questionNumber];
+          if (slug) {
+            const url = `https://leetcode.com/problems/${slug}/`;
+            chrome.tabs.create({ url });
+          } else {
+            alert('Question number not found.');
+          }
+        })
+        .catch(error => {
+          console.error('Error loading questions.json:', error);
+          alert('Failed to load question data.');
+        });
+    } else {
+      alert('Please enter a question number.');
+    }
+  });
